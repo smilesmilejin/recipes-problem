7d439505967d7da4b74034681ad0024f8cd222c4
Ansel Rognlie <ansel@adadevelopersacademy.org>
2024-12-13 14:09:21 -0800

    fixes spacing

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ def most_varied(recipes):
 
 
 recipes_1 = [
-  ("Burrito", "Sam", ("Beef", "Cheese", "Tortilla")),
-  ("Hot Dish", "Amy", ("Tater tots", "Chicken Cream", "Cheese", "Pepper")),
-  ("Stew", "Xinting", ("Beef", "Onion", "Tomato", "Carrot")),
-  ("Taco", "Sam", ("Tortilla", "Cheese", "Beef")),
-  ("Chalupa", "Sam", ("Tortilla", "Beef", "Cheese")),
-  ("Latkes", "Hallie", ("Potato", "Oil")),
-  ("Pea Soup", "Xinting", ("Peas", "Onion", "Carrot", "Chicken Stock")),
+    ("Burrito", "Sam", ("Beef", "Cheese", "Tortilla")),
+    ("Hot Dish", "Amy", ("Tater tots", "Chicken Cream", "Cheese", "Pepper")),
+    ("Stew", "Xinting", ("Beef", "Onion", "Tomato", "Carrot")),
+    ("Taco", "Sam", ("Tortilla", "Cheese", "Beef")),
+    ("Chalupa", "Sam", ("Tortilla", "Beef", "Cheese")),
+    ("Latkes", "Hallie", ("Potato", "Oil")),
+    ("Pea Soup", "Xinting", ("Peas", "Onion", "Carrot", "Chicken Stock")),
 ]
 assert most_varied(recipes_1) == [
-  ("Xinting", ["Beef", "Carrot", "Chicken Stock", "Onion", "Peas", "Tomato"]), 
-  ("Amy", ["Cheese", "Chicken Cream", "Pepper", "Tater tots"])
+    ("Xinting", ["Beef", "Carrot", "Chicken Stock", "Onion", "Peas", "Tomato"]), 
+    ("Amy", ["Cheese", "Chicken Cream", "Pepper", "Tater tots"])
 ]
 
 recipes_2 = [
-  ("Latkes", "Hallie", ("Potato", "Oil")),
-  ("Chalupa", "Sam", ("Tortilla", "Beef", "Cheese")),
+    ("Latkes", "Hallie", ("Potato", "Oil")),
+    ("Chalupa", "Sam", ("Tortilla", "Beef", "Cheese")),
 ]
 assert most_varied(recipes_2) == [
-  ("Sam", ["Beef", "Cheese", "Tortilla"]),
-  ("Hallie", ["Oil", "Potato"])
+    ("Sam", ["Beef", "Cheese", "Tortilla"]),
+    ("Hallie", ["Oil", "Potato"])
 ]
 
 print("All test cases passed!")
