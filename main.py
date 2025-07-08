57d30c0d7b8eab78d70261c84a29e62081311ff9
Xinshuang Jin <jinxinshuang@gmail.com>
2025-07-08 14:57:14 -0400

    2025/07/08 Interview Prep Method 3

--- main.py
+++ main.py
@@ -62,7 +62,52 @@
 #     return result 
 
 
-# Method 2
+# # Method 2
+# def most_varied(recipes):
+
+#     chef_ingrediants = {}
+#     for recipe_name, chef, ingredients in recipes:
+#         if chef in chef_ingrediants:
+#             chef_ingrediants[chef].update(set(ingredients))
+#         elif chef not in chef_ingrediants:
+#             chef_ingrediants[chef] = set()
+#             chef_ingrediants[chef].update(set(ingredients))
+
+#     print(chef_ingrediants)
+
+#     # sort a dictionary based on the length of its values, descending: reverse=True
+#     # sorting a dictionary will give you a new one (usually as a list of tuples 
+#     sorted_chef_ingrediants = sorted(chef_ingrediants.items(), key=lambda ingredients: len(ingredients[1]), reverse=True)
+    
+#     # [
+#     #     ('Xinting', {'Carrot', 'Onion', 'Beef', 'Chicken Stock', 'Tomato', 'Peas'}), 
+#     #     ('Amy', {'Pepper', 'Tater tots', 'Cheese', 'Chicken Cream'}), 
+#     #     ('Sam', {'Tortilla', 'Cheese', 'Beef'}), 
+#     #     ('Hallie', {'Oil', 'Potato'})
+#     # ]
+    
+#     print(sorted_chef_ingrediants)
+
+#     # top two ingreditans
+#     top_two_ingrediants = []
+
+#     count = 0
+#     index = 0
+
+#     while count < 2:
+#         chef = sorted_chef_ingrediants[index][0]
+#         sorted_ingredients = sorted(sorted_chef_ingrediants[index][1])
+#         top_two_ingrediants.append((chef, sorted_ingredients))
+#         count += 1
+#         index += 1
+    
+#     print(top_two_ingrediants)
+
+
+#     return top_two_ingrediants
+
+
+# Method 3: use .sort
 def most_varied(recipes):
 
     chef_ingrediants = {}
@@ -76,9 +121,12 @@ def most_varied(recipes):
     print(chef_ingrediants)
 
     # sort a dictionary based on the length of its values, descending: reverse=True
-    # sorting a dictionary will give you a new one (usually as a list of tuples 
-    sorted_chef_ingrediants = sorted(chef_ingrediants.items(), key=lambda ingredients: len(ingredients[1]), reverse=True)
-    
+    sorted_chef_ingrediants = list(chef_ingrediants.items())
+    # print(sorted_chef_ingredients)
+
+    sorted_chef_ingrediants.sort(key=lambda item: len(item[1]), reverse=True)
+    # print(sorted_chef_ingredients)
+
     # [
     #     ('Xinting', {'Carrot', 'Onion', 'Beef', 'Chicken Stock', 'Tomato', 'Peas'}), 
     #     ('Amy', {'Pepper', 'Tater tots', 'Cheese', 'Chicken Cream'}), 
@@ -91,18 +139,13 @@ def most_varied(recipes):
     # top two ingreditans
     top_two_ingrediants = []
 
-    count = 0
-    index = 0
+    top_1_chef = sorted_chef_ingrediants[0][0]
+    top_1_sorted_ingredients = sorted(sorted_chef_ingrediants[0][1])
+    top_two_ingrediants.append((top_1_chef, top_1_sorted_ingredients))
 
-    while count < 2:
-        chef = sorted_chef_ingrediants[index][0]
-        sorted_ingredients = sorted(sorted_chef_ingrediants[index][1])
-        top_two_ingrediants.append((chef, sorted_ingredients))
-        count += 1
-        index += 1
-    
-    print(top_two_ingrediants)
-
+    top_2_chef = sorted_chef_ingrediants[1][0]
+    top_2_sorted_ingredients = sorted(sorted_chef_ingrediants[1][1])
+    top_two_ingrediants.append((top_2_chef, top_2_sorted_ingredients))
 
     return top_two_ingrediants
 
